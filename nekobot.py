0d8c8b77a37db3e0453f0eb82ed63bb6d35600eb
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-17 00:46:19 +0530

    rip fakegs

--- nekobot.py
+++ nekobot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from validators.url import url
 st.title('Kanna Says!!')
 text = st.text_input("What do you want kanna to say?\n")
@@ -15,19 +15,3 @@ else:
         f.write(requests.get(a).content)
     img = Image.open("temp.png").convert("RGB")
     st.image(img)
-    
-search = st.text_input("Add text for search bar!")
-result = st.text_input("Add text for result bar!")
-if search and result !="":
-    img = Image.open("resources/fakegs.jpg")
-    drawing = ImageDraw.Draw(img)
-    blue = (0, 0, 255)
-    black = (0, 0, 0)
-    font1 = ImageFont.truetype(font="resources/ProductSans-BoldItalic.ttf", size=20)
-    font2 = ImageFont.truetype(font="resources/helpers/styles/ProductSans-Light.ttf", size=23)
-    drawing.text((450, 258), result, fill=blue, font=font1)
-    drawing.text((270, 37), search, fill=black, font=font2)
-    img.save("temp.jpg")
-    st.image(img)
-else:
-    st.write("Maybe type something?")
