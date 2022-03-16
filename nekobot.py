4ee181096da9b3a9ddf6e6002a5ba0fcca697c31
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-17 00:51:06 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -2,16 +2,19 @@ import streamlit as st
 import requests
 from PIL import Image
 from validators.url import url
-st.title('Kanna Says!!')
-text = st.text_input("What do you want kanna to say?\n")
-if text == '':
-    st.write("Type something!!")
+if st.button("Kanna!")
+    st.title('Kanna Says!!')
+    text = st.text_input("What do you want kanna to say?\n")
+    if text == '':
+        st.write("Type something!!")
+    else:
+        r = requests.get(f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
+    ).json()
+        a = r.get("message")
+        iurl = url(a)
+        with open("temp.png", "wb") as f:
+            f.write(requests.get(a).content)
+        img = Image.open("temp.png").convert("RGB")
+        st.image(img)
 else:
-    r = requests.get(f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
-).json()
-    a = r.get("message")
-    iurl = url(a)
-    with open("temp.png", "wb") as f:
-        f.write(requests.get(a).content)
-    img = Image.open("temp.png").convert("RGB")
-    st.image(img)
+    st.write("Meow")
