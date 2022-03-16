e81f9d4e72da9da20606a902ead1c2bfa47044db
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-17 01:02:33 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -3,19 +3,16 @@ import requests
 from PIL import Image
 from validators.url import url
 st.title("Hello there!")
-if st.button("Kanna!"):
-    st.title('Kanna Says!!')
-    text = st.text_input("What do you want kanna to say?\n")
-    if text == '':
-        st.write("Type something!!")
-    else:
-        r = requests.get(f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
-    ).json()
-        a = r.get("message")
-        iurl = url(a)
-        with open("temp.png", "wb") as f:
-            f.write(requests.get(a).content)
-        img = Image.open("temp.png").convert("RGB")
-        st.image(img)
+st.title('Kanna Says!!')
+text = st.text_input("What do you want kanna to say?\n")
+if text == '':
+    st.write("Type something!!")
 else:
-    st.write("Meow")
+    r = requests.get(f"https://nekobot.xyz/api/imagegen?type=kannagen&text={text}"
+).json()
+    a = r.get("message")
+    iurl = url(a)
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(a).content)
+    img = Image.open("temp.png").convert("RGB")
+    st.image(img)
