23d38be7074ac5b831769239985f36945196b6db
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 00:02:01 +0530

    Update phub.py

--- phub.py
+++ phub.py
@@ -3,8 +3,8 @@ import streamlit as st
 from PIL import Image, ImageMath
 from validators.url import url
 from telegraph import upload_file
-text = st.file_uploader("Enter the image you want to use!")
-if text != None and text.endswith('png','jpg','jpeg'):
+text = st.file_uploader(type=['png', 'jpg','jpeg'],"Enter the image you want to use!")
+if text != None:
        xt = Image.open(text)
        xt.save("mp.png")
        response = upload_file("mp.png")
