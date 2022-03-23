509612bc1a70392d27ff74469abea393fe16a6ed
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 00:26:18 +0530

    Update fakegs.py

--- fakegs.py
+++ fakegs.py
@@ -2,13 +2,10 @@ import streamlit as st
 import requests
 from PIL import Image, ImageDraw, ImageFont
 from validators.url import url
-from io import BytesIO
 search = st.text_input("Add text for search bar!")
 result = st.text_input("Add text for result bar!")
 if search and result != "":
     a = "https://i.imgur.com/wNFr5X2.jpg"
-    response = requests.get(a)
-    a = Image.open(BytesIO(response.content))
     with open("temp.jpg", "wb") as f:
         f.write(requests.get(a).content)
     img = Image.open("temp.jpg")
