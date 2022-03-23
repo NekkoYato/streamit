1d19c23e69aa0f2ef204776caefacec5573a679d
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-23 23:52:08 +0530

    Update phub.py

--- phub.py
+++ phub.py
@@ -4,10 +4,13 @@ from PIL import Image, ImageMath
 from validators.url import url
 from telegraph import upload_file
 text = st.file_uploader("Enter the image you want to use!")
-xt = Image.open(text)
-xt.save("mp.jpg")
-response = upload_file("mp.jpg")
-text1 = f"https://telegra.ph{response[0]}"
+if text != None:
+    xt = Image.open(text)
+    xt.save("mp.png")
+    response = upload_file("mp.png")
+    text1 = f"https://telegra.ph{response[0]}"
+else:
+    st.write("Upload some image!")
 text3 = st.text_input("Enter username!!")
 text2 = st.text_input("Enter the comment you want!")
 if text1 and text2 and text3 != "":    
@@ -21,7 +24,7 @@ if text1 and text2 and text3 != "":
     img = Image.open("temp.png")
     if img.mode != "RGB":
         img = img.convert("RGB")
-    img.save("temp.jpg", "jpeg")
+    img.save("temp.png", "png")
     st.image(img)
 else:
     st.write("well, try something interesting hehe")
