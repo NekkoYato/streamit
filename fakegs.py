1e3afa334024d934fffec7a11622fb0b1ccf77d2
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 00:19:31 +0530

    Update fakegs.py

--- fakegs.py
+++ fakegs.py
@@ -5,9 +5,12 @@ from validators.url import url
 search = st.text_input("Add text for search bar!")
 result = st.text_input("Add text for result bar!")
 if search and result != "":
-    imgurl = "https://i.imgur.com/wNFr5X2.jpg"
+    r = requests.get(f"https://i.imgur.com/wNFr5X2.jpg"
+).json()
+    a = r.get("message")
+    iurl = url(a)
     with open("temp.jpg", "wb") as f:
-        f.write(requests.get(imgurl).content)
+        f.write(requests.get(a).content)
     img = Image.open("temp.jpg")
     drawing = ImageDraw.Draw(img)
     blue = (0, 0, 255)
