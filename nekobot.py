3e071bb24658aec1e501cbd3dee8b62126b4d615
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 23:57:27 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -3,8 +3,12 @@ st.markdown("<h1 style='text-align: center; color: white;'>OwO, Hi There!</h1>",
 st.markdown("<h1 style='text-align: center; color: white; font-size: medium;'>Nekobot! web based implementation of nekobot api</h1>", unsafe_allow_html=True)
 m = st.markdown("""
 <style>
-div.stButton > button:first-child {
-    background-color: rgb(204, 49, 49);
+div.stButton > button:first-child { 
+color: #4F8BF9;
+border-radius: 20%;
+backgroud-color: #00ff00;
+height: 3em;
+width: 3em; 
 }
 </style>""", unsafe_allow_html=True)
 
