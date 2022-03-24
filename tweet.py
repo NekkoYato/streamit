fce021d2eef00a7525edc34a1f5da595e3dfca31
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 12:04:25 +0530

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -5,8 +5,9 @@ from validators.url import url
 text1 = st.text_input("Enter the username!\n")
 text2 = st.text_input("What do you want to tweet?\n")
 if text1 and text2 != '':
-    r = requests.get(f"https://nekobot.xyz/api/imagegen?type=tweet&text={text2}&username{text1}"
-).json()
+    r = requests.get(
+        f"https://nekobot.xyz/api/imagegen?type=tweet&text={text2}&username={text1}"
+    ).json()
     a = r.get("message")
     iurl = url(a)
     with open("temp.png", "wb") as f:
