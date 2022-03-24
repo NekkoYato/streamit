c96e7fd89a104bb538ad6442715f8b92881b1e11
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 22:46:42 +0530

    well well well

---
+++ nekobot.py
@@ -0,0 +1,10 @@
+import streamlit as st
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://ibb.co/NVGV2Wh");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
