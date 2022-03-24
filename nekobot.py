04c5215c536258617d3be6072e9d33c882ab814b
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 23:05:41 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -1,39 +1,14 @@
 import streamlit as st
 st.markdown(
     """
-<style>
-.reportview-container .markdown-text-container {
-    font-family: monospace;
-}
-.sidebar .sidebar-content {
-    background-image: linear-gradient(#2e7bcf,#2e7bcf);
-    color: white;
-}
-.Widget>label {
-    color: white;
-    font-family: monospace;
-}
-[class^="st-b"]  {
-    color: white;
-    font-family: monospace;
-}
-.st-bb {
-    background-color: transparent;
-}
-.st-at {
-    background-color: #0c0080;
-}
-footer {
-    font-family: monospace;
-}
-.reportview-container .main footer, .reportview-container .main footer a {
-    color: #0c0080;
-}
-header .decoration {
-    background-image: none;
-}
-
-</style>
-""",
-    unsafe_allow_html=True,
+    <style>
+    .reportview-container {
+        background: url("https://i.pinimg.com/originals/04/ca/fd/04cafd620c31e929492a155da2169cab.gif")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://www.linkpicture.com/q/1193275_1.jpg")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
