f2a59dcbddaa41087f1e7042ddb45cb08696a8de
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 22:56:33 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -1,10 +1,24 @@
 import streamlit as st
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://www.linkpicture.com/q/1193275_1.jpg");
-background-size: cover;
-}
-</style>
-'''
-st.markdown(page_bg_img, unsafe_allow_html=True)
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('resources/1193275.jpg')
