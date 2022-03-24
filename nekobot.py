cec8a50d84bd11ecdb0987aeb9be2c6c770b3b5d
Yato <89446086+NekkoYato@users.noreply.github.com>
2022-03-24 22:49:38 +0530

    Update nekobot.py

--- nekobot.py
+++ nekobot.py
@@ -2,7 +2,7 @@ import streamlit as st
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://ibb.co/NVGV2Wh");
+background-image: url("https://www.linkpicture.com/q/1193275_1.jpg");
 background-size: cover;
 }
 </style>
